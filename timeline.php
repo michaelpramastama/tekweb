18b788b4dd7418a5914f3a11faf152b6b6aea699
michaelpramastama <michaelwidap@gmail.com>
2020-05-13 21:02:20 +0700

    css

--- timeline.php
+++ timeline.php
@@ -6,13 +6,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
 
         <title>SOSMED PETRA</title>
     </head>
 
     <body>
         <!-- navbar -->
-        <nav class="navbar navbar-expand-lg navbar-light " style="background-color: #e3f2fd;">
+        <nav class="navbar fixed-top navbar-expand-lg navbar-light " style="background-color: #e3f2fd;">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -34,7 +35,12 @@
                 </ul>
             </div>
         </nav>
+        <!-- timeline -->
+        <div class="body-timeline">
+            <div class="posting-timeline">
 
+            </div>
+        </div>
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
